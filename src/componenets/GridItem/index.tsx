7d89fbdf5cfd0f055123e9c53ac507c9d8a03933
Vinicius Francisco Alves <vinicius.francis.alves@gmail.com>
2022-05-11 23:33:26 -0300

    first commit

---
+++ src/componenets/GridItem/index.tsx
@@ -0,0 +1,24 @@
+import * as C from './styles'
+import { GridItemType } from '../../types/GridItemType'
+import b75vg from '../../svgs/b7.svg'
+import { items } from '../../data/items'
+
+type Props = {
+    item: GridItemType,
+    onClick: () => void
+}
+export const GridItem = ({ item, onClick}: Props) => {
+    return (
+        <C.Container 
+        showBackground={item.permanentShown || item.shown}
+        onClick={onClick}
+        >
+            {!item.permanentShown === false && item.shown === false &&
+                <C.Icon src={b75vg} alt="" opacity={.1} />
+            }
+            {item.permanentShown || item.shown && item.item !== null &&
+               <C.Icon src={items[item.item].icon} alt="" />
+            }
+        </C.Container>
+    )
+}
